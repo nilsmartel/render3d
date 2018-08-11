b0f711c5e7087218d9fabe0cd6534fff3f7b6e02
sombrastudios <nils.martel@concepts-system.com>
2018-08-11 18:38:38 +0200

    Changed the way Matrix3x3 Type is declared

--- src/main.rs
+++ src/main.rs
@@ -34,15 +34,15 @@ impl Mul<f32> for Vector3 {
     }
 }
 
-struct Matrix3x3 {
-    value: [f32; 9]
-}
+struct Matrix3x3(Vector3, Vector3, Vector3);
 
 impl Matrix3x3 {
     fn identity() -> Matrix3x3 {
-        Matrix3x3{value: [1.0, 0.0, 0.0,
-                          0.0, 1.0, 0.0,
-                          0.0, 0.0, 1.0]}
+        Matrix3x3(
+            Vector3(1.0, 0.0, 0.0),
+            Vector3(0.0, 1.0, 0.0),
+            Vector3(0.0, 0.0, 1.0)
+        )
     }
 }
 
@@ -51,19 +51,19 @@ impl Mul<Vector3> for Matrix3x3 {
 
     fn mul(self, v: Vector3) -> Self::Output {
         Vector3(
-            v.0 * self.value[0],
-            v.1 * self.value[1],
-            v.2 * self.value[2]
+            v.0 * (self.0).0,
+            v.1 * (self.1).1,
+            v.2 * (self.2).2
         ) + 
         Vector3(
-            v.0 * self.value[3],
-            v.1 * self.value[4],
-            v.2 * self.value[5]
+            v.0 * (self.0).0,
+            v.1 * (self.1).1,
+            v.2 * (self.2).2
         ) + 
         Vector3(
-            v.0 * self.value[6],
-            v.1 * self.value[7],
-            v.2 * self.value[8]
+            v.0 * (self.0).0,
+            v.1 * (self.1).1,
+            v.2 * (self.2).2
         )
     }
 }
