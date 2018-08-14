69d2d112388ce0085555a322b38c508156a08972
sombrastudios <nils.martel@concepts-system.com>
2018-08-14 15:01:25 +0200

    Get Vector2 to be a Generic Type

--- src/lina.rs
+++ src/lina.rs
@@ -1,5 +1,3 @@
-
-
 use std::ops::{Add, Mul};
 
 #[derive(Debug, Copy, Clone)]
@@ -15,11 +13,7 @@ impl Add for Vector3 {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Vector3(
-            self.0 + other.0,
-            self.1 + other.1,
-            self.2 + other.2
-        )
+        Vector3(self.0 + other.0, self.1 + other.1, self.2 + other.2)
     }
 }
 
@@ -27,36 +21,27 @@ impl Mul<f32> for Vector3 {
     type Output = Self;
 
     fn mul(self, scalar: f32) -> Self {
-        Vector3(
-            self.0 * scalar,
-            self.1 * scalar,
-            self.2 * scalar
-        )
+        Vector3(self.0 * scalar, self.1 * scalar, self.2 * scalar)
     }
 }
 
 #[derive(Debug, Copy, Clone)]
-pub struct Vector2(f32, f32);
+pub struct Vector2<T>(T, T);
 
-impl Vector2 {
-    pub fn new(x: f32, y: f32) -> Self {
+impl<T> Vector2<T> {
+    pub fn new(x: T, y: T) -> Self {
         Vector2(x, y)
     }
 }
 
-impl Add for Vector2 {
-    type Output = Vector2;
+impl<T> Add for Vector2<T>
+where
+    T: Add<T>,
+{
+    type Output = Vector2<<T as Add>::Output>;
 
-    fn add(self, other: Vector2) -> Vector2 {
+    fn add(self, other: Vector2<T>) -> Self::Output {
         Vector2(self.0 + other.0, self.1 + other.1)
-    }
-}
-
-impl Mul<f32> for Vector2 {
-    type Output = Self;
-
-    fn mul(self, scalar: f32) -> Self {
-        Vector2(self.0 * scalar, self.1 * scalar)
     }
 }
 
@@ -67,30 +52,18 @@ impl Matrix3x3 {
         Matrix3x3(
             Vector3(1.0, 0.0, 0.0),
             Vector3(0.0, 1.0, 0.0),
-            Vector3(0.0, 0.0, 1.0)
+            Vector3(0.0, 0.0, 1.0),
         )
     }
 }
 
-impl<'a > Mul<Vector3> for &'a Matrix3x3 {
+impl<'a> Mul<Vector3> for &'a Matrix3x3 {
     type Output = Vector3;
 
     fn mul(self, v: Vector3) -> Self::Output {
-        Vector3(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1,
-            v.2 * (self.2).2
-        ) +
-        Vector3(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1,
-            v.2 * (self.2).2
-        ) +
-        Vector3(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1,
-            v.2 * (self.2).2
-        )
+        Vector3(v.0 * (self.0).0, v.1 * (self.1).1, v.2 * (self.2).2)
+            + Vector3(v.0 * (self.0).0, v.1 * (self.1).1, v.2 * (self.2).2)
+            + Vector3(v.0 * (self.0).0, v.1 * (self.1).1, v.2 * (self.2).2)
     }
 }
 
@@ -98,46 +71,24 @@ impl Mul<Vector3> for Matrix3x3 {
     type Output = Vector3;
 
     fn mul(self, v: Vector3) -> Self::Output {
-        Vector3(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1,
-            v.2 * (self.2).2
-        ) +
-        Vector3(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1,
-            v.2 * (self.2).2
-        ) +
-        Vector3(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1,
-            v.2 * (self.2).2
-        )
+        Vector3(v.0 * (self.0).0, v.1 * (self.1).1, v.2 * (self.2).2)
+            + Vector3(v.0 * (self.0).0, v.1 * (self.1).1, v.2 * (self.2).2)
+            + Vector3(v.0 * (self.0).0, v.1 * (self.1).1, v.2 * (self.2).2)
     }
 }
 
-pub struct Matrix2x2(Vector2, Vector2);
+pub struct Matrix2x2(Vector2<f32>, Vector2<f32>);
 
 impl Matrix2x2 {
     pub fn identity() -> Matrix2x2 {
-        Matrix2x2(
-            Vector2(1.0, 0.0),
-            Vector2(0.0, 1.0)
-        )
+        Matrix2x2(Vector2(1.0, 0.0), Vector2(0.0, 1.0))
     }
 }
 
-impl Mul<Vector2> for Matrix2x2 {
-    type Output = Vector2;
+impl Mul<Vector2<f32>> for Matrix2x2 {
+    type Output = Vector2<f32>;
 
-    fn mul(self, v: Vector2) -> Self::Output {
-        Vector2(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1
-        ) +
-        Vector2(
-            v.0 * (self.0).0,
-            v.1 * (self.1).1
-        )
+    fn mul(self, v: Vector2<f32>) -> Self::Output {
+        Vector2(v.0 * (self.0).0, v.1 * (self.1).1) + Vector2(v.0 * (self.0).0, v.1 * (self.1).1)
     }
 }
--- src/mesh.rs
+++ src/mesh.rs
@@ -1,21 +1,25 @@
-
 use lina;
 
 pub struct Mesh {
     vertecise: Vec<lina::Vector3>,
     normals: Vec<lina::Vector3>,
-    uv_coordinates: Vec<lina::Vector2>,
-    faces: Vec<FaceIDs>
+    uv_coordinates: Vec<lina::Vector2<f32>>,
+    faces: Vec<FaceIDs>,
 }
 
 impl Mesh {
     fn new(
         vertecise: Vec<lina::Vector3>,
         normals: Vec<lina::Vector3>,
-        uv_coordinates: Vec<lina::Vector2>,
-        faces: Vec<FaceIDs>
-        ) -> Mesh {
-        Mesh { vertecise, normals, uv_coordinates, faces }
+        uv_coordinates: Vec<lina::Vector2<f32>>,
+        faces: Vec<FaceIDs>,
+    ) -> Mesh {
+        Mesh {
+            vertecise,
+            normals,
+            uv_coordinates,
+            faces,
+        }
     }
 
     pub fn transform(&self, matrix: &lina::Matrix3x3) -> Mesh {
@@ -23,8 +27,8 @@ impl Mesh {
             self.vertecise.iter().map(|v| matrix * *v).collect(),
             self.normals.iter().map(|v| matrix * *v).collect(),
             self.uv_coordinates.clone(),
-            self.faces.clone()
-            )
+            self.faces.clone(),
+        )
     }
 }
 
@@ -32,23 +36,29 @@ impl Mesh {
 struct FaceIDs {
     vert_id: usize,
     normal_id: usize,
-    uv_id: usize
+    uv_id: usize,
 }
 
 pub struct Face {
     pub vertex: lina::Vector3,
     pub normal: lina::Vector3,
-    pub uv_coordinate: lina::Vector2
+    pub uv_coordinate: lina::Vector2<f32>,
 }
 
 impl Face {
     pub fn new(
         vertex: lina::Vector3,
         normal: lina::Vector3,
-        uv_coordinate: lina::Vector2
-        ) -> Face { Face {vertex, normal, uv_coordinate} }   
+        uv_coordinate: lina::Vector2<f32>,
+    ) -> Face {
+        Face {
+            vertex,
+            normal,
+            uv_coordinate,
+        }
+    }
 }
- 
+
 impl<'a> IntoIterator for &'a Mesh {
     type IntoIter = MeshIterator<'a>;
     type Item = Face;
@@ -56,14 +66,14 @@ impl<'a> IntoIterator for &'a Mesh {
     fn into_iter(self) -> MeshIterator<'a> {
         MeshIterator {
             mesh: self,
-            current: 0
+            current: 0,
         }
     }
 }
 
 pub struct MeshIterator<'a> {
     mesh: &'a Mesh,
-    current: usize
+    current: usize,
 }
 
 impl<'a> Iterator for MeshIterator<'a> {
@@ -73,11 +83,11 @@ impl<'a> Iterator for MeshIterator<'a> {
         if self.current < self.mesh.faces.len() {
             let ids = &self.mesh.faces[self.current];
             self.current += 1;
-            
+
             return Some(Face::new(
                 self.mesh.vertecise[ids.vert_id],
                 self.mesh.normals[ids.normal_id],
-                self.mesh.uv_coordinates[ids.uv_id]
+                self.mesh.uv_coordinates[ids.uv_id],
             ));
         }
 
