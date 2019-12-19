b9b58c270cb1481738c458090f2de7fbec41468d
Nils Martel <nils.martel@graphmasters.net>
2019-12-19 14:42:45 +0100

    Include Token type for Wavefront parsing

---
+++ src/parse/wavefront.rs
@@ -0,0 +1,12 @@
+enum Token {
+    Comment(String),
+    Mtllib(String),
+    ObjectName(String),
+    V(f64, f64, f64),
+    Vt(f64, f64),
+    Vn(f64, f64, f64),
+    Group(String),
+    Usemtl(String),
+    Smoothness(Option<u8>),
+    Face(Option<usize>, Option<usize>, Option<usize>),
+}
